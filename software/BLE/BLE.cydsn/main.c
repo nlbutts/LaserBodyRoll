5b5d0abb1c8e1b72c11d11fb71ee3cccb19b3078
Nicholas Butts <nlbutts@ieee.org>
2018-03-11 21:17:17 -0500

    Tweaks to the BLE module to get it to work.

--- software/BLE/BLE.cydsn/main.c
+++ software/BLE/BLE.cydsn/main.c
@@ -13,12 +13,23 @@
 
 int main()
 {
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    CyGlobalIntEnable; /* Uncomment this line to enable global interrupts. */
 
-    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    PWM_GREEN_Start();
+
+    CyBle_ProcessEvents();
+
+
+    
     for(;;)
     {
+        CyBle_ProcessEvents();
         /* Place your application code here. */
+        BLUE_LED_Write(0);
+        CyDelay(1000);
+        BLUE_LED_Write(1);
+        CyDelay(1000);
     }
 }
 
