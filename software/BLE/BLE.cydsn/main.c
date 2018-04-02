7b66c51346f1a02c37f6a3c3a0cd261ff8e99a8c
Nicholas Butts <nlbutts@ieee.org>
2018-04-02 07:32:48 -0500

    Updated Buck layout.

--- software/BLE/BLE.cydsn/main.c
+++ software/BLE/BLE.cydsn/main.c
@@ -16,20 +16,34 @@ int main()
     CyGlobalIntEnable; /* Uncomment this line to enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    PWM_GREEN_Start();
+    //SPI_Start();
 
     CyBle_ProcessEvents();
 
-
+    volatile uint32_t rxBufSize;
+    volatile uint32_t txBufSize;
+    
     
     for(;;)
     {
-        CyBle_ProcessEvents();
+        //rxBufSize = SPI_SpiUartGetRxBufferSize();        
+        //txBufSize = SPI_SpiUartGetTxBufferSize();
+        //if (rxBufSize > 0)
+        //{
+            //SPI_SpiUartWriteTxData(SPI_SpiUartReadRxData());
+        //}
+        
+        CyBle_ProcessEvents();        
         /* Place your application code here. */
-        BLUE_LED_Write(0);
-        CyDelay(1000);
-        BLUE_LED_Write(1);
-        CyDelay(1000);
+        //BLUE_LED_Write(0);
+        //CyDelay(1000);
+        //BLUE_LED_Write(1);
+        //CyDelay(1000);
+
+        
+        BLUE_LED_Write(SCLK_Read());
+        GREEN_LED_Write(MOSI_Read());
+        RED_LED_Write(MISO_Read());
     }
 }
 
