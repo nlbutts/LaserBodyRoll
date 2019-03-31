aa548b28e9fdc2e4879edb9c044611c5529e44b7
Nick Butts <nlbutts@ieee.org>
2019-03-31 17:54:21 -0500

    Updated the test_can library.

--- software/python/test_can.py
+++ software/python/test_can.py
@@ -5,17 +5,18 @@ import time
 import spidev
 
 class MCP25625():
-   def __init__(self, clk):
+   def __init__(self, clk, max_speed_hz):
       self.clk = clk
       self.reset = LED(18)
       self.TxReq = LED(14)
       self.CanInt = Button(24)
       self.spi = spidev.SpiDev()
       self.spi.open(0, 0)
+      self.spi.max_speed_hz = max_speed_hz
 
    def setMode(self, mode):
       value = mode << 5 | 0x7
-      self.spi.xfer2([0x20, 0x0F, value])
+      self.spi.xfer2([0x05, 0x0F, 0xFF, value])
 
    def resetChip(self):
       self.reset.off()
@@ -28,26 +29,36 @@ class MCP25625():
       self.TxReq.off()
 
    def printStatus(self):
+      # Read CANCTRL
       ret = self.spi.xfer2([0x03, 0x0F, 0x00])
       control_reg = ret[2]
+      # Read Status
       ret = self.spi.xfer2([0xA0, 0x00, 0x00])
       status = ret[2]
+      # Read Rx Status
       ret = self.spi.xfer2([0xB0, 0x00, 0x00])
       rx_status = ret[2]
+      # Read CANSTAT
+      ret = self.spi.xfer2([0x03, 0x0E, 0x00])
+      canstat = ret[2]
+      # Read TEC and REC
+      ret = self.spi.xfer2([0x03, 0x1C, 0x00, 0x00])
+      TEC = ret[2]
+      REC = ret[3]
 
-      print("CANCTRL: {:X} - STATUS: {:X} - RX_STATUS: {:X}".format(control_reg, status, rx_status))
+      print("CANCTRL: {:X} - CANSTATUS: {:X} - STATUS: {:X} - RX_STATUS: {:X} - TEC: {:} - REC: {:}".format(control_reg, canstat, status, rx_status, TEC, REC))
 
    def configure(self, PRSEG, PHSEG1, PHSEG2):
       # Write CNF1
-      ret = self.spi.xfer2([0x20, 0x2A, 0x00])
+      ret = self.spi.xfer2([0x05, 0x2A, 0xFF, 0x00])
       # Write CNF2
       value = (PHSEG1 - 1) << 3 | (PRSEG - 1)
-      ret = self.spi.xfer2([0x20, 0x29, value])
+      ret = self.spi.xfer2([0x05, 0x29, 0xFF, value])
       # Write CNF3
       value = PHSEG2 - 1
-      ret = self.spi.xfer2([0x20, 0x28, value])
+      ret = self.spi.xfer2([0x05, 0x28, 0xFF, value])
       # Use B0RTSM to transmit a CAN message
-      ret = self.spi.xfer2([0x20, 0x0D, 0x01])
+      ret = self.spi.xfer2([0x05, 0x0D, 0xFF, 0x01])
 
    def configure_rx(self):
       # Set Receive acceptance mask to 0
@@ -58,13 +69,14 @@ class MCP25625():
 
    def setTxObject(self, id, data):
       buf = []
-      buf.append(0x40)
-      buf.append(id >> 3)
-      buf.append((id & 0x08) << 5)
+      buf.append(0x02)
+      buf.append(0x31)
+      buf.append((id >> 3) & 0xFF)
+      buf.append((id & 0x07) << 5)
       buf.append(0)
       buf.append(0)
       buf.append(0x08)
-     
+
       # Data
       buf.append(data[0])
       buf.append(data[1])
@@ -76,19 +88,63 @@ class MCP25625():
       buf.append(data[7])
       self.spi.xfer2(buf)
 
+   def printRxObject(self, obj):
+      buf = []
+      buf.append(0x03)
+      buf.append(0x61 | obj << 5)
+      for i in range(13):
+         buf.append(0)
+      ret = self.spi.xfer2(buf)
+      canid = ret[2] << 3
+      canid |= (ret[3] >> 5) & 0x7
+      length = ret[6] & 0xF
+      formatStr = "{:X} - ".format(canid)
+      for i in range(0, length):
+         formatStr = "{:} {:X}".format(formatStr, ret[7 + i])
 
-can = MCP25625(8000000)
+      print(formatStr)
+
+   def rxMessageAvailable(self):
+      # Read Rx Status
+      ret = self.spi.xfer2([0xB0, 0x00, 0x00])
+      rx_status = ret[2]
+      if (rx_status & 0x80):
+         return 0
+      elif (rx_status & 0x40):
+         return 1
+      return -1
+
+   def printAddress(self, address, length):
+      buf = []
+      buf.append(0x03)
+      buf.append(address)
+      for i in range(length):
+         buf.append(0)
+
+      ret = self.spi.xfer2(buf)
+      temp = ""
+      for v in ret:
+         temp = "{:} {:X}".format(temp, v)
+
+      print(temp)
+
+
+can = MCP25625(8000000, 1000000)
 can.resetChip()
 can.printStatus()
 can.setMode(4)
-can.printStatus()
 can.configure(5, 8, 2)
 can.configure_rx()
-can.printStatus()
-can.setMode(2)
+can.setMode(0)
 can.setTxObject(0x123, [0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF])
+can.printAddress(0x30, 16)
+can.printStatus()
 
 while True:
    can.printStatus()
    time.sleep(1)
    can.transmit()
+   rxObjAvailable = can.rxMessageAvailable()
+   if rxObjAvailable >= 0:
+      print("Rx message received object {:}".format(rxObjAvailable))
+      can.printRxObject(rxObjAvailable)
